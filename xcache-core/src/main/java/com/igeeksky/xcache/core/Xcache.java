ff2f5ee99a024663c8ab4f494d28ee76e9908a3f
tonylau08 <TONYLAU@TONYLAU-PC>
2017-02-21 19:47:22 +0800

    add commnet

--- xcache-core/src/main/java/com/igeeksky/xcache/core/Xcache.java
+++ xcache-core/src/main/java/com/igeeksky/xcache/core/Xcache.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Tony.lau All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.igeeksky.xcache.core;
 
 import org.springframework.cache.Cache;
@@ -6,6 +22,10 @@ import org.springframework.util.Assert;
 
 import com.igeeksky.xcache.support.CacheKey;
 
+/**
+ * @author Tony.Lau
+ * @createTime 2017-02-21 18:23:40
+ */
 public class Xcache implements Cache {
 	
 	private CacheKey cacheKey;
--- xcache-core/src/main/java/com/igeeksky/xcache/core/XlocalCache.java
+++ xcache-core/src/main/java/com/igeeksky/xcache/core/XlocalCache.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Tony.lau All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.igeeksky.xcache.core;
 
 import java.util.Iterator;
@@ -13,7 +29,12 @@ import org.springframework.util.Assert;
 import com.igeeksky.dcafe.snowflake.TimeGen;
 import com.igeeksky.xcache.support.CacheKey;
 
-
+/**
+ * 
+ * @author Tony.Lau
+ * @blog: https://my.oschina.net/xcafe
+ * @createTime 2017-02-21 18:38:43
+ */
 public class XlocalCache implements Cache {
 
 	private final ConcurrentMap<Object, LocalValueWrapper> store;
